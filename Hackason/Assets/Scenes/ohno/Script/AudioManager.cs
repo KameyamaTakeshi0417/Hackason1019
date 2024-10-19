0c1debb9872ae9c6511b5b7b7b14a0670b086fd1
TOCelty <3dmodelingfighter@gmail.com>
2024-10-19 20:40:52 +0900

    マスタ―ボリューム２

--- Hackason/Assets/Scenes/ohno/Script/AudioManager.cs
+++ Hackason/Assets/Scenes/ohno/Script/AudioManager.cs
@@ -21,29 +21,69 @@ public class AudioManager : MonoBehaviour
         sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
         bgmSlider.value = PlayerPrefs.GetFloat("BGMVolume", 0.75f);
         systemSlider.value = PlayerPrefs.GetFloat("SystemVolume", 0.75f);
+
+        // 初期値を反映するために、スライダーの値を即座に適用
+        SetMasterVolume(masterSlider.value);
+        SetBGMVolume(bgmSlider.value);
+        SetSFXVolume(sfxSlider.value);
+        SetSystemVolume(systemSlider.value);
+
+        // スライダーにリスナーを追加
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        bgmSlider.onValueChanged.AddListener(SetBGMVolume);
+        systemSlider.onValueChanged.AddListener(SetSystemVolume);
     }
 
     public void SetMasterVolume(float volume)
     {
+        if (volume == 0)
+    {
+        audioMixer.SetFloat("MasterVolume", -80f);  // 無音に設定
+    }
+    else
+    {
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);  // デシベル変換
+    }
         PlayerPrefs.SetFloat("MasterVolume", volume);  // 設定を保存
     }
 
     public void SetSFXVolume(float volume)
     {
+         if (volume == 0)
+    {
+        audioMixer.SetFloat("SFXVolume", -80f);  // 無音に設定
+    }
+    else
+    {
         audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+    }
         PlayerPrefs.SetFloat("SFXVolume", volume);
     }
 
     public void SetBGMVolume(float volume)
     {
+        if (volume == 0)
+    {
+        audioMixer.SetFloat("BGMVolume", -80f);  // 無音に設定
+    }
+    else
+    {
         audioMixer.SetFloat("BGMVolume", Mathf.Log10(volume) * 20);
+    }
         PlayerPrefs.SetFloat("BGMVolume", volume);
     }
 
     public void SetSystemVolume(float volume)
     {
+        if (volume == 0)
+    {
+        audioMixer.SetFloat("SFXVolume", -80f);  // 無音に設定
+    }
+    else
+    {
         audioMixer.SetFloat("SystemVolume", Mathf.Log10(volume) * 20);
+    }
         PlayerPrefs.SetFloat("SystemVolume", volume);
     }
 }
